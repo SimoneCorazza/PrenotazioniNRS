c542b9253406df3cd283c505e40f4b3fc1ae4482
Simone Corazza <>
2025-03-18 22:44:01 +0100

    BE - refactor della conversione dei responsabili

--- backend/Infrastructure/Persistence/ContestoDbContext.cs
+++ backend/Infrastructure/Persistence/ContestoDbContext.cs
@@ -15,37 +15,30 @@
         {
         }
 
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
+                .Properties<Responsabili>()
+                .HaveMaxLength(500)
+                .HaveConversion<ResponsabiliConverter>();
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             var pulizieSede = modelBuilder.Entity<PuliziaSede>();
-
             pulizieSede.HasKey(x => new { x.NumeroSettimana, x.Anno });
             pulizieSede.Property(x => x.NumeroSettimana)
                 .ValueGeneratedNever();
             pulizieSede.Property(x => x.Anno)
                 .ValueGeneratedNever();
-
-            pulizieSede.Property(x => x.Responsabili)
-                .HasConversion(
-                    x => string.Join("|", x.Select(x => x.ToString())),
-                    x => new Responsabili(x.Split("|", StringSplitOptions.RemoveEmptyEntries).Select(x => new Responsabile(x))))
-                .HasMaxLength(500);
+            pulizieSede.Property(x => x.Responsabili);
 
             var attivitaOrdinarie = modelBuilder.Entity<AttivitaOrdinaria>();
-
             attivitaOrdinarie.HasKey(x => x.Giorno);
             attivitaOrdinarie.Property(x => x.Giorno)
                 .ValueGeneratedNever();
-            attivitaOrdinarie.Property(x => x.ResponsabiliApertura)
-                .HasConversion(
-                    x => string.Join("|", x.Select(x => x.ToString())),
-                    x => new Responsabili(x.Split("|", StringSplitOptions.RemoveEmptyEntries).Select(x => new Responsabile(x))))
-                .HasMaxLength(500);
-            attivitaOrdinarie.Property(x => x.ResponsabiliChiusura)
-                .HasConversion(
-                    x => string.Join("|", x.Select(x => x.ToString())),
-                    x => new Responsabili(x.Split("|", StringSplitOptions.RemoveEmptyEntries).Select(x => new Responsabile(x))))
-                .HasMaxLength(500);
+            attivitaOrdinarie.Property(x => x.ResponsabiliApertura);
+            attivitaOrdinarie.Property(x => x.ResponsabiliChiusura);
             attivitaOrdinarie.Property(x => x.Stato);
         }
     }
---
+++ backend/Infrastructure/Persistence/ResponsabiliConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using PrenotazioniNRS.Domain.Sede;
+
+namespace PrenotazioniNRS.Infrastructure.Persistence
+{
+    public class ResponsabiliConverter : ValueConverter<Responsabili, string>
+    {
+        public ResponsabiliConverter()
+            : base(x => string.Join("|", x.Select(x => x.ToString())),
+                x => new Responsabili(x.Split("|", StringSplitOptions.RemoveEmptyEntries).Select(x => new Responsabile(x))))
+        {
+        }
+    }
+}
