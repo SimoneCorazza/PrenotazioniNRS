fe715454017a0410acd39ad1f0c7465285fc4ca9
Simone Corazza <>
2025-03-18 23:25:12 +0100

    FE - fix typo

--- frontend/src/components/Attivita.tsx
+++ frontend/src/components/Attivita.tsx
@@ -3,12 +3,12 @@ import Responsabili from './Responsabili';
 import AperturaIcon from './icons/Apertura';
 import ChiusuraIcon from './icons/Chiusura';
 
-interface AttivitaPros {
+interface AttivitaProps {
     responsabiliChiusura: string[];
     responsabiliApertura: string[];
 }
 
-const Attivita: React.FC<AttivitaPros> = ({responsabiliApertura, responsabiliChiusura}) => {
+const Attivita: React.FC<AttivitaProps> = ({responsabiliApertura, responsabiliChiusura}) => {
 
     return (
         <div className='attivita'>
